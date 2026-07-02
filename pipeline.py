f23af11893667186bd9ae732a206cdf8e64cf02e
Siddharth Shenoy <sidshenoy2000@gmail.com>
2026-07-02 12:29:27 -0400

    Initial commit - implement core RAG pipeline with UAV papers in knowledge base

---
+++ pipeline.py
@@ -0,0 +1,113 @@
+import os
+from pypdf import PdfReader
+from sentence_transformers import SentenceTransformer
+import chromadb
+from litellm import completion
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from hashlib import sha256
+from dotenv import load_dotenv
+
+# folder = os.fsencode(os.getenv('DIR_PATH'))
+
+load_dotenv()
+
+client = chromadb.PersistentClient(path="./db")
+text_embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
+
+gemini_api_key = os.getenv('GEMINI_API_KEY')
+
+
+def extract_text_from_pdf(pdf_file=None):
+    all_text = ""
+    reader = PdfReader(f'{os.getenv('DIR_PATH')}/{pdf_file}')
+    for page in reader.pages:
+        all_text += page.extract_text() or ""
+    return all_text, reader.metadata
+
+
+def process_text_and_store(all_text, metadata):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500, chunk_overlap=50, separators=["\n\n", "\n", " ", ""], length_function=len
+    )
+
+    hash_value = sha256(all_text.encode('utf-8')).hexdigest()
+    chunks = text_splitter.split_text(all_text)
+
+    knowledge_collection = client.get_or_create_collection(name="knowledgeBase")
+    metadata_collection = client.get_or_create_collection(name="metadata", embedding_function=None)
+
+    # check if paper has already been added to knowledge base
+    results = metadata_collection.get(ids=[hash_value])
+
+    if results["ids"]:
+        print("Paper already exists in knowledge base")
+        return
+
+    title = metadata.title if metadata.title else ""
+    publication_id = metadata["/IEEE Publication ID"] if "/IEEE Publication ID" in metadata.keys() else ""
+    issue_id = metadata["/IEEE Issue ID"] if "/IEEE Issue ID" in metadata.keys() else ""
+    article_id = metadata["/IEEE Article ID"] if "/IEEE Article ID" in metadata.keys() else ""
+
+    metadata_collection.add(
+        ids=[hash_value],
+        documents=[hash_value],
+        metadatas=[{"title": title, "publication_id": publication_id, "issue_id": issue_id, "article_id": article_id}]
+    )
+
+    for i, chunk in enumerate(chunks):
+        embedding = text_embedding_model.encode(chunk)
+        knowledge_collection.add(
+            ids=[f"{hash_value}_chunk_{i}"],
+            embeddings=[embedding.tolist()],
+            metadatas=[{"source": hash_value, "chunk_id": i}],
+            documents=[chunk.join(f'Title: {title}')]
+        )
+
+
+def semantic_search(query, collection, top_k=5):
+    query_embedding = text_embedding_model.encode(query)
+    results = collection.query(
+        query_embeddings=[query_embedding.tolist()], n_results=top_k
+    )
+    return results
+
+
+def generate_response(query, context):
+    prompt = f"Query: {query}\nContext: {context}"
+    response = completion(
+        model="gemini/gemini-3.5-flash",
+        messages=[{"content": "You are an academic expert who has read all the papers provided in the context. Answer the given query based on your knowledge from these papers ONLY. Do NOT use external resources. Cite the paper title based on the context provided in a readable format. The title is at the end of each text chunk with the key 'Title:'. Provide a percentage of how confident you are of your answer.", "role": "system"}, {"content": prompt, "role": "user"}],
+        api_key=gemini_api_key
+    )
+    return response['choices'][0]['message']['content']
+
+
+def get_source_metadata(sources, collection):
+    source_ids = list({
+        paper['source']
+        for paper in sources['metadatas'][0]
+    })
+    source_metadata = collection.get(ids=source_ids)
+    return source_metadata
+
+
+# for file in os.listdir(folder):
+#     filename = os.fsdecode(file)
+#     pdf_text, pdf_metadata = extract_text_from_pdf(filename)
+#     process_text_and_store(all_text=pdf_text, metadata=pdf_metadata)
+
+
+# knowledge_collection = client.get_or_create_collection(name="knowledgeBase")
+# results = knowledge_collection.get()
+#
+# print(results)
+
+knowledge_collection = client.get_or_create_collection(name="knowledgeBase")
+metadata_collection = client.get_or_create_collection(name="metadata", embedding_function=None)
+query = input("Please ask a question about UAVs: ")
+results = semantic_search(query, knowledge_collection)
+# source_metadata = get_source_metadata(results, metadata_collection)
+context = "\n".join(results['documents'][0])
+# source_context = "\n".join(source_metadata)
+response = generate_response(query, context)
+print(response)
